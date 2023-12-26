bf5705112aeb00495c773bcab12396f4575b603c
Antonio Silva <antoniopsilva083@gmail.com>
2023-12-25 21:45:22 -0300

    start project

---
+++ src/main/java/br/com/antoniosilva/spring/picpay/challenge/SpringPicPayChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.antoniosilva.spring.picpay.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPicPayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPicPayChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/antoniosilva/spring/picpay/challenge/SpringPicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antoniosilva.spring.picpay.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
